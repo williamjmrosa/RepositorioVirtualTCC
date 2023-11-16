edd3530c07c1294f58d52291f6f59a3f2e638a30
William José <williamjmrosa@gmail.com>
2023-11-16 20:15:41 -0300

    Menu do botão login mostra onde usuario vai

--- menuLogin/menuLogin.php
+++ menuLogin/menuLogin.php
@@ -1,9 +1,21 @@
 <?php
+session_start();
+include_once '../Modelo/aluno.class.php';
+include_once '../Modelo/professor.class.php';
+include_once '../Modelo/visitante.class.php';
+include_once '../Modelo/bibliotecario.class.php';
+include_once '../Modelo/adm.class.php';
 
+if(isset($_SESSION['usuario'])) {
+    $user = unserialize($_SESSION['usuario']);
+    $tipo = get_class($user);
+}
 ?>
 <li>
-    <a class="btn fundo-secundario fw-bold m-1" href="#"><img id="img-login" class="me-4" src="../img/login.png" />Login</a>
+    <a class="btn fundo-secundario fw-bold m-1" href="<?php echo isset($user) ? '../visao/perfil.php' : '../visao/telaLogin.php'; ?>"><i class="bi bi-person-circle me-2"></i><?php echo isset($user) ? explode(' ', $user->nome)[0] : 'Login'; ?></a>
+    <?php if(isset($user)) {?>
     <ul class="fundo-secundario p-2 fw-bold text-start">
+        <?php if($tipo == 'Bibliotecario' || $tipo == 'Adm') {?>
         <li>
             <a href="../visao/telaCadastroCurso.php">Cadastrar Curso</a>
         </li>
@@ -20,17 +32,26 @@
             <a href="../visao/telaCadastroProfessor.php">Cadastrar Professor</a>
         </li>
         <li>
-            <a href="../visao/telaCadastroVisitante.php">Cadastrar Visitante</a>
-        </li>
-        <li>
             <a href="../visao/telaCadastroBibliotecario.php">Cadastrar Bibliotecário</a>
         </li>
         <li>
-            <a href="../visao/telaCadastroAdm.php">Cadastrar Administrador</a>
+            <a href="../visao/telaCadastroVisitante.php">Cadastrar Visitante</a>
         </li>
         <li>
             <a href="../visao/telaCadastroTCC.php">Cadastrar TCC</a>
         </li>
+        <?php
+        }
+        if($tipo == 'Adm'){?>
+        <li>
+            <a href="../visao/telaCadastroAdm.php">Cadastrar Administrador</a>
+        </li>
+        <?php
+        }?>
+        <li>
+            <a href="../controle/login-controle.php?OP=6">Logout <i class="bi bi-box-arrow-right"></i></a>
+        </li>
     </ul>
-
+    <?php
+        }?>
 </li>
