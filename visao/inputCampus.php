8d719af2b637b71139da3b65631ae8dda95d4e58
William José <williamjmrosa@gmail.com>
2023-11-16 18:45:09 -0300

    ajuste

--- visao/inputCampus.php
+++ visao/inputCampus.php
@@ -2,21 +2,16 @@
 include_once '../dao/campusdao.class.php';
 include_once '../Modelo/campus.class.php';
 
-if(isset($_GET['BUSCA'])){
+if (isset($_GET['BUSCA'])) {
     $busca = filter_var($_GET['BUSCA'], FILTER_SANITIZE_SPECIAL_CHARS);
 
     $campusDAO = new CampusDAO();
-    if($busca != ""){
-        $campi = $campusDAO->buscarCampusAluno($busca);
+    if ($busca != "") {
+        $campus = $campusDAO->buscarCampusAluno($busca);
 
-        foreach($campi as $campus){
-            echo '<label for="campusNome" class="form-label">Campus</label>';
-            echo '<input type="hidden" id="campus" name="campus" value="' . $campus->idcampus . '">';
-            echo '<input type="text" disabled class="form-control" id="campusNome" placeholder="Campus" value="' . $campus->nome . '">';
-        }
-
+        echo '<label for="campusNome" class="form-label">Campus</label>';
+        echo '<input type="hidden" id="campus" name="campus" value="' . $campus->idcampus . '">';
+        echo '<input type="text" disabled class="form-control" id="campusNome" placeholder="Campus" value="' . $campus->nome . '">';
+        
     }
-
 }
-
-?>
