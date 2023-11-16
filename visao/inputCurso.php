001223cb6b6d344f16f00bac30c18dd81a6f3283
William José <williamjmrosa@gmail.com>
2023-11-16 18:55:57 -0300

    ajuste

--- visao/inputCurso.php
+++ visao/inputCurso.php
@@ -2,17 +2,14 @@
 include_once '../dao/cursodao.class.php';
 include_once '../Modelo/curso.class.php';
 
-if(isset($_GET['BUSCA'])){
+if (isset($_GET['BUSCA'])) {
     $busca = filter_var($_GET['BUSCA'], FILTER_SANITIZE_SPECIAL_CHARS);
 
     $cursoDAO = new CursoDAO();
 
-    foreach($cursoDAO->buscarCursoAluno($busca) as $curso){
-        echo '<label for="cursoNome" class="form-label">Curso</label>';
-        echo '<input type="hidden" id="curso" name="curso" value="' . $curso->idcurso . '">';
-        echo '<input type="text" disabled class="form-control" id="cursoNome" placeholder="Curso" value="' . $curso->nome . '">';
-    }
+    $curso = $cursoDAO->buscarCursoAluno($busca);
     
+    echo '<label for="cursoNome" class="form-label">Curso</label>';
+    echo '<input type="hidden" id="curso" name="curso" value="' . $curso->idcurso . '">';
+    echo '<input type="text" disabled class="form-control" id="cursoNome" placeholder="Curso" value="' . $curso->nome . '">';
 }
-
-?>
