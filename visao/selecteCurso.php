97f4295194185b03d64212981ecb16639aea5a8d
William José <williamjmrosa@gmail.com>
2024-11-14 18:05:59 -0300

    adicionando buscar curso pelo nome

--- visao/selecteCurso.php
+++ visao/selecteCurso.php
@@ -17,6 +17,19 @@ if(isset($_GET['ID'])){
     }else{
         echo "<option value=''>Nenhum Campus cadastrado</option>";
     }
+}else if(isset($_GET['BUSCA'])){
+    $busca = filter_var($_GET['BUSCA'], FILTER_SANITIZE_SPECIAL_CHARS);
+
+    $cursoDAO = new CursoDAO();
+    $cursos = $cursoDAO->buscarCursosPorNome($busca);
+        echo "<option value='null' selected>Selecione um Curso</option>";
+    if(is_array($cursos)){
+        foreach ($cursos as $curso) {
+            echo "<option onclick='lista(this)' value='$curso->idCurso'>$curso->nome | ". $curso->mostrarEnsino()."</option>";
+        }
+    }else{
+        echo "<option value=''>Nenhum Curso cadastrado</option>";
+    }
 }
 
 
